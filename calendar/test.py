9f4848bf015b5dde9f783fe51e2f222e9caab577
Fabio (MacbookAir) <faacferraz@gmail.com>
2021-07-31 02:15:47 -0400

    Internal push by script

--- calendar/test.py
+++ calendar/test.py
@@ -1,4 +1,13 @@
 from RgbMatrix import RgbMatrix
+import time
+import sys
 
 matrix = RgbMatrix()
-matrix.render_img('../dog2.png')
+matrix.render_img('./img/cloud.gif')
+try:
+    print("Press CTRL-C to stop.")
+    while True:
+        time.sleep(100)
+except KeyboardInterrupt:
+    sys.exit(0)
+
