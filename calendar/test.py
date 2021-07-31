a891d2cbbb318237b82ffed4ce6382bcaadcb614
Fabio (MacbookAir) <faacferraz@gmail.com>
2021-07-31 02:18:07 -0400

    Internal push by script

--- calendar/test.py
+++ calendar/test.py
@@ -3,11 +3,10 @@ import time
 import sys
 
 matrix = RgbMatrix()
-matrix.render_img('./img/cloud.gif')
+matrix.render_img('./img/icon/et.gif')
 try:
     print("Press CTRL-C to stop.")
     while True:
         time.sleep(100)
 except KeyboardInterrupt:
     sys.exit(0)
-
