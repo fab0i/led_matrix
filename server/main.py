28a7e431ebe6b90c398415cf5d928c02af0d94ef
Fabio Ferraz <faacferraz@gmail.com>
2021-09-27 03:14:01 -0400

    Update main.py

--- server/main.py
+++ server/main.py
@@ -27,4 +27,4 @@ class ImageDisplay(Resource):
 api.add_resource(ImageDisplay, '/')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
